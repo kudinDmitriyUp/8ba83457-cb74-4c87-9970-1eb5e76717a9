dc6731a486fa5e7dece6a1bce39fc269b4893ee1
bender <kudin.dima@gmail.com>
2026-06-07 23:18:55 +0000

    Switch to version 6: modified src/pages/HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,12 +5,22 @@
 import React from 'react';
 import HomeSection from './HomePage/sections/Home';
 import AboutSection from './HomePage/sections/About';
-import ProductsSection from './HomePage/sections/Products';
+import * as ProductsModule from './HomePage/sections/Products';
 import ServicesSection from './HomePage/sections/Services';
 import TestimonialsSection from './HomePage/sections/Testimonials';
 import PartnersSection from './HomePage/sections/Partners';
 import FaqSection from './HomePage/sections/Faq';
 import ContactSection from './HomePage/sections/Contact';
+
+const ProductsSection = () => {
+  const Component = 
+    (ProductsModule as any).default || 
+    (ProductsModule as any).ProductsSection || 
+    (ProductsModule as any).Products || 
+    (Object.keys(ProductsModule).length > 0 ? (ProductsModule as any)[Object.keys(ProductsModule)[0]] : null);
+  
+  return Component ? <Component /> : null;
+};
 
 export default function HomePage(): React.JSX.Element {
   return (
